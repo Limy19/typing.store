fac30af8762346d0c5cd81c7158043223ac350a0
lmambl <lmambl@mail.ru>
2023-12-21 11:40:50 +0300

    feat: slider

--- client/src/features/ProductList/components/ProductPage.tsx
+++ client/src/features/ProductList/components/ProductPage.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router';
-import Slider from 'react-slick';
-import UppDateCard from './UppDateCard';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 import { deleteProduct, initProductOne } from '../productSlice';
 import type { RootState } from '../../../store/store';
 import { add } from '../../CartPage/cartSlice';
@@ -27,7 +29,6 @@ function ProductPage(): JSX.Element {
   const buy = (): void => {
     void dispatch(add(product.id));
   };
-
   return (
     <div className="about">
       <div
@@ -41,20 +42,22 @@ function ProductPage(): JSX.Element {
           marginBottom: '25px',
         }}
       >
-        {product?.PhotoProducts.map((photo) => (
-          <img
-            style={{
-              width: '85%',
-              height: '45%',
-              position: 'static',
-              marginBottom: '10px',
-              objectFit: 'cover',
-            }}
-            alt="pupupu"
-            src={photo.img}
-            key={photo.id}
-          />
-        ))}
+        <Carousel>
+          {product?.PhotoProducts.map((photo) => (
+            <img
+              style={{
+                width: '65%',
+                height: '45%',
+                position: 'static',
+                marginBottom: '10px',
+                objectFit: 'cover',
+              }}
+              alt="pupupu"
+              src={photo.img}
+              key={photo.id}
+            />
+          ))}
+        </Carousel>
       </div>
       <div className="textabout" style={{ width: '50%' }}>
         <h1
